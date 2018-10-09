e85b7c514c13ecccbd9910d865ae7e926bd42095
Isaac <isaac.r.watts@gmail.com>
2018-10-09 10:15:49 -0600

    on click events added for toggling

---
+++ src/app/task.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+	Tasks:Task[] = [];
+
+	constructor() { }
+
+	getCompleteTasks() {
+		return this.Tasks.filter(task => task.done);
+	}
+
+	getIncompleteTasks() {
+		return this.Tasks.filter(task => !task.done);
+	}
+
+	addTask(task:Task) {
+		this.Tasks.push(task);
+	}
+}
+
+interface Task {
+	desc:string
+	done:boolean
+}
